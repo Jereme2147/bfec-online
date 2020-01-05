11c56fe4981a4475252df357686bada0a4f3fbb7
jereme <jereme.daniels@gmail.com>
2020-01-05 11:33:48 -0500

    small formatting edit to page-singe

--- single-post.php
+++ single-post.php
@@ -1,18 +1,19 @@
 <?php
     get_header();
 ?>
+<!-- <h1>single-post</h1> -->
 <div class="banner-div">
     <!-- dynamic should be loaded from WP -->
-    <img src="<?php echo get_template_directory_uri()?>/img/about-1500.png" class="desktop-img"alt="">
-    <img src="<?php echo get_template_directory_uri()?>/img/about-600.png"class="mobile-img" alt="">
+    <img src="<?php echo get_template_directory_uri()?>/img/about-1500x590.jpg" class="desktop-img"alt="">
+    <img src="<?php echo get_template_directory_uri()?>/img/about-1500x590.jpg"class="mobile-img" alt="">
 </div>
 <?php
 
       while(have_posts()) {
             the_post();
 ?>
-    <div class="no-banner-spacer">
-    </div>
+    <!-- <div class="no-banner-spacer">
+    </div> -->
     <section class="single-section">
         <div>
         <?php 
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
     get_header();
 ?>
-<!-- <h1>single</h1> -->
+<h1>single</h1>
 <?php
       while(have_posts()) {
             the_post();
